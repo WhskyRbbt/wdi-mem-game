df17b041769045c5540bca2afceca060a83eef69
Jonathan Zelaya <WhskyRbbt@Jonathans-MacBook-Air.local>
2020-01-29 08:53:59 -0800

    created game logic

---
+++ js/main.js
@@ -0,0 +1,58 @@
+const cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+const cardsInPlay = [];
+
+
+function checkForMatch() {
+		if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] == cardsInPlay[1])
+			alert("You found a match!")	
+ 		else 
+			alert("Sorry, try again.")
+		}
+
+}
+function flipCard() {
+	
+	let cardId = this.getAttribute("data-id");
+
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute("src", cards[cardId].cardImage);
+	if(cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+
+}
+
+var createBoard = function() {
+    	cards.forEach(function card(i) {
+        	var cardElement = document.createElement('img');
+        	cardElement.setAttribute("img", "images/back.png");
+        	cardElement.setAttribute("data-id", i);
+           	cardElement.addEventListener("click", flipCard(i))
+           	document.getElementById("game-board").appendChild(cardElement)
+           })
+       };
+
+createBoard();
